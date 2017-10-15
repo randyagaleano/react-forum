fdd3f379775a8adad71992e6e39def934c14b46c
Randy Galeano <randyagaleano@gmail.com>
2017-10-15 19:25:22 -0400

    basic html laid out, functionality up next
    basic html and css skeleton created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="panel panel-default">
+        <div className="panel panel-default post-body">
           <div className="panel-body">
-            hello world
+            Hello, i'm a post.
+          </div>
+        </div>
+        <div className="panel panel-default post-editor">
+          <div className="panel-body">
+            <textarea className="form-control post-editor-input" />
+            <button className="btn btn-success post-editor-button">Post</button>
           </div>
         </div>
       </div>
